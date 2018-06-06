1691322799fc97ffdabf861dd33c3ea93cc0f9d5
CK Kisiday <Kisiday2486M@ckstudents.org>
2018-06-06 08:30:16 -0700

    Adding this all to a git repo because my teacher made me.

---
+++ src/TwitterDriver.java
@@ -0,0 +1,57 @@
+/**
+ * Twitter Driver and Client
+ * 
+ * @author Ria Galanos
+ * @author Tony Potter
+ * Original idea by Ria Galanos, whose documentation and source can be found at
+ * https://github.com/riagalanos/cs1-twitter
+ * 
+ **/
+import twitter4j.TwitterException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+   public class TwitterDriver
+   {
+      private static PrintStream consolePrint;
+   
+      public static void main (String []args) throws TwitterException, IOException
+      {
+         // set up classpath and properties file
+             
+         Twitterer bigBird = new Twitterer(consolePrint);
+
+         // Problem 1         
+         // Create and set a String called message here
+      
+         String message = "I'm testing out the twitter4j API for Java.  Thanks @cscheerleader! "
+                        + "You can find out more at https://github.com/riagalanos/cs1-twitter";
+           bigBird.tweetOut(message);
+
+         // Problem 2
+         // Choose a public Twitter user's handle
+         // and analyze their tweets
+         
+   
+         //Scanner scan = new Scanner(System.in);
+         //System.out.print("Please enter a Twitter handle, do not include the '@' symbol (or 'done' to quit.)");
+         //String twitter_handle = scan.next();
+         //while (!"done".equals(twitter_handle))
+         //{
+          //   bigBird.queryHandle(twitter_handle);
+         //    System.out.print("Please enter a Twitter handle, do not include the '@' symbol (or 'done' to quit.)");
+          //   twitter_handle = scan.next();
+             
+         //}
+         
+         // Problem 3
+         // Find all Spurs-related tweets in a 20 mile radius around San Antonio
+         // in the past day.
+         bigBird.saQuery("Spurs");
+
+      }//main         
+         
+   }//class    
+         
+   
---
+++ src/Twitterer.java
@@ -0,0 +1,76 @@
+import twitter4j.GeoLocation;       // jar found at http://twitter4j.org/en/index.html
+import twitter4j.Paging;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.TwitterException;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Twitterer
+   {
+      private Twitter twitter;
+      private PrintStream consolePrint;
+      private List<Status> statuses;
+
+     
+      public Twitterer(PrintStream console)
+      {
+         // Makes an instance of Twitter - this is re-useable and thread safe.
+         // Connects to Twitter and performs authorizations.
+         twitter = TwitterFactory.getSingleton(); 
+         consolePrint = console;
+         statuses = new ArrayList<Status>();
+      }
+   
+     /******************  Part 1 *******************/
+     /** 
+      * This method tweets a given message.
+      * @param String  a message you wish to Tweet out
+      */
+      public void tweetOut(String message) throws TwitterException, IOException
+      {
+          Status status = twitter.updateStatus(message);
+          System.out.println("Successfully updated status to {" + status.getText() + "]");
+      }
+   
+      
+     /******************  Part 2 *******************/
+     /** 
+      * This method queries the tweets of a particular user's handle.
+      * @param String  the Twitter handle (username) without the @sign
+      */
+      @SuppressWarnings("unchecked")
+      public void queryHandle(String handle) throws TwitterException, IOException
+      {
+   
+      }
+   	
+     /** 
+      * This helper method fetches the most recent 2,000 tweets of a particular user's handle and 
+      * stores them in an arrayList of Status objects.  Populates statuses.
+      * @param String  the Twitter handle (username) without the @sign
+      */
+      private void fetchTweets(String handle) throws TwitterException, IOException
+      {
+    
+       }   
+    
+     /******************  Part 3 *******************/
+     /** 
+      * This method finds the last 100 queries in the San Antonio area since yesterday.
+      * Lat/Long for San Antonio is 29.4241° N, 98.4936° W (west is negative.)
+      * @param searchTerm the term to search for.
+      */
+      public void saQuery (String searchTerm)
+      {
+    
+       }
+   
+   }  
